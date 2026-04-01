7c9d9d326d12c1b35bf57b9b033b189b3e429c6b
rameem2003 <rameem2019@gmail.com>
2026-04-01 16:28:52 +0600

    Blog rest api complete

--- src/main/java/com/rameem/blogapi/controllers/BlogController.java
+++ src/main/java/com/rameem/blogapi/controllers/BlogController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/blog")
@@ -22,5 +23,35 @@ public class BlogController {
     public String createBlog(@RequestBody BlogModel newBLog){
         blogRepository.save(newBLog);
         return "A Blog is Created";
+    }
+
+    @PutMapping("/update/{id}")
+    public String updateBlog(@PathVariable Long id, @RequestBody BlogModel newBlog ){
+ List<BlogModel> allBlogs = blogRepository.findAll();
+
+     for(BlogModel blog : allBlogs){
+         if(blog.getId().equals(id)){
+             blog.setTitle(newBlog.getTitle());
+             blog.setContent(newBlog.getContent());
+
+             blogRepository.save(blog);
+
+             break;
+
+         }else {
+             return "Blog not found";
+
+         }
+     }
+
+        return "A Blog is Updated";
+
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteBlog(@PathVariable Long id){
+        blogRepository.deleteById(id);
+        return "Id " + id + " has been deleted";
+
     }
 }
